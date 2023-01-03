208993e64e44471a5fdf787b33bd8d78c82a51c6
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-01-03 23:20:05 +0500

    feat: added category-module

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,103 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  HttpException,
+  Delete,
+  Param,
+  Get,
+  Put,
+} from '@nestjs/common';
+import { UpdateResult } from 'typeorm';
+import {
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiOperation,
+} from '@nestjs/swagger';
 
-@Controller('category')
-export class CategoryController {}
+import { CreateCategory, UpdateCategory } from './dto';
+import { CategoryService } from './category.service';
+
+@ApiTags('Category')
+@Controller('categories')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get('/id')
+  @ApiOperation({ summary: 'Method: returns category by id' })
+  @ApiOkResponse({
+    description: 'The category was returned successfully',
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @HttpCode(HttpStatus.OK)
+  async getById(@Param('id') id: string) {
+    return this.categoryService.getById(id);
+  }
+
+  @Get('/')
+  @ApiOperation({ summary: 'Method: returns all categories' })
+  @ApiOkResponse({
+    description: 'The categories were returned successfully',
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @HttpCode(HttpStatus.OK)
+  async getData() {
+    try {
+      return await this.categoryService.getAll();
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Post('/')
+  @ApiOperation({ summary: 'Method: creates new category' })
+  @ApiCreatedResponse({
+    description: 'The category was created successfully',
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() categoryData: CreateCategory) {
+    try {
+      return await this.categoryService.create(categoryData);
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Put('/:id')
+  @ApiOperation({ summary: 'Method: updating category' })
+  @ApiOkResponse({
+    description: 'Category was changed',
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @HttpCode(HttpStatus.OK)
+  async changeData(
+    @Body() userData: UpdateCategory,
+    @Param('id') id: string,
+  ): Promise<UpdateResult> {
+    try {
+      return await this.categoryService.update(userData, id);
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Delete('/:id')
+  @ApiOperation({ summary: 'Method: deleting category' })
+  @ApiOkResponse({
+    description: 'Category was deleted',
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteData(@Param('id') id: string) {
+    try {
+      return await this.categoryService.delete(id);
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
