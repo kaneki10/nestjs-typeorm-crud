e79a41b762d60a64a86670066c11a435ec3e139b
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-01-04 19:38:44 +0500

    feat: cutegory with sub category reletion added

--- src/modules/sub-category/sub-category.entity.ts
+++ src/modules/sub-category/sub-category.entity.ts
@@ -1,4 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Category } from '../category/category.entity';
 
 @Entity({ name: 'sub_category' })
 export class SubCategory extends BaseEntity {
@@ -11,4 +19,11 @@ export class SubCategory extends BaseEntity {
     ru: string;
     en: string;
   };
+
+  @ManyToOne(() => Category, (category) => category.subCategories, {
+    cascade: true,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  category: Category;
 }
